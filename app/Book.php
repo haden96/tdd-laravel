7a5278e2c676711c089137d0b8038aa6bcb470dc
haden96 <charkiewiczdaniel@gmail.com>
2020-07-26 19:39:33 +0200

    adds models

--- app/Book.php
+++ app/Book.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Reservation;
 
 class Book extends Model
 {
@@ -18,5 +19,30 @@ class Book extends Model
         $this->attributes['author_id'] = (Author::firstOrCreate([
             'name' => $author,
         ]))->id;
+    }
+
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id'=>$user->id,
+            'checked_out_at'=>now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
+    }
+
+    protected function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
